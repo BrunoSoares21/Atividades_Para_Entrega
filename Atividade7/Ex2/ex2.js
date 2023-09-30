8f9070d1e4cfbe78941672b5bfc2fa038d813fc9
Bruno Soares <brunosoaresdasilva25@gmail.com>
2023-09-30 18:32:53 -0300

    subindo ex2.js

---
+++ Atividade7/Ex2/ex2.js
@@ -0,0 +1,7 @@
+/*
+Crie uma classe Cliente, essa classe deve ter nome, telefone, 
+renda e e-mail como atributos e um método verificarCredito(), 
+esse método deve retornar uma promise que retorna: Crédito aprovado 
+(renda > 2000) ou Crédito recusado (renda < 2000)
+*/
+
